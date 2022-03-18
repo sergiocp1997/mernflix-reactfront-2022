e259ca037d4d7bce5d71ce3e9c72fc939b7669ea
sergiocp1997 <sergio.canadillas@alu.uclm.es>
2022-03-18 18:53:38 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Login from './Login';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
