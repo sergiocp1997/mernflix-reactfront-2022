08f8f2055b34a3d55449c3dc0bdacabbe76bdae6
sergiocp1997 <sergio.canadillas@alu.uclm.es>
2022-03-18 20:13:15 +0100

    cambios

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Login from './Login';
+import MovieList from './movies/MovieList';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Login/>} />
+          <Route path="/home" element={<MovieList/>} />
         </Routes>
       </div>
     </Router>
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     baseURL_API: process.env.REACT_APP_APIURI,
     clientID: process.env.REACT_APP_CLIENTID
-}
-
-export default config
+ }
+  
+export default config;
